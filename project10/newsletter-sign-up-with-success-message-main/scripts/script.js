895078e2d0bc1639a2040155e664895c69448d32
Philipp Schnitzer <phillippschnitzer@hotmail.com>
2024-08-02 21:59:50 +0200

    finish validation

--- project10/newsletter-sign-up-with-success-message-main/scripts/script.js
+++ project10/newsletter-sign-up-with-success-message-main/scripts/script.js
@@ -3,21 +3,44 @@ const submitButton = document.querySelector('.submit-button')
 const dissmissButton = document.querySelector('.dissmiss-button')
 const newsletter = document.getElementById('newsletter')
 const newsletter_2 = document.getElementById('newsletter-2')
+const errorMessage = document.querySelector('.error')
+const inputEmail = document.getElementById('email')
 
-form.addEventListener('submit', (e) => {
-  handleSubmit(e)
-})
-
-submitButton.addEventListener('click', () => {
-  newsletter.classList.add('hide')
-  newsletter_2.classList.remove('hide')
-})
+form.addEventListener('submit', handleSubmit)
 
 dissmissButton.addEventListener('click', () => {
   newsletter.classList.remove('hide')
   newsletter_2.classList.add('hide')
 })
 
-function handleSubmit(event) {
-  event.preventDefault()
+function handleSubmit(e) {
+  e.preventDefault()
+
+  const data = Object.fromEntries(new FormData(e.target))
+
+  validateEmail(data.email) ? showSuccess() : showError()
+}
+
+function validateEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+  if (!emailRegex.test(email)) {
+    return false
+  }
+  return true
+}
+
+function showSuccess() {
+  newsletter.classList.add('hide')
+  newsletter_2.classList.remove('hide')
+  errorMessage.classList.add('hide')
+}
+
+function showError() {
+  errorMessage.classList.remove('hide')
+  email.style.backgroundColor = 'rgb(228, 83, 63)'
+  setTimeout(() => {
+    errorMessage.classList.add('hide')
+    email.style.backgroundColor = 'white'
+  }, 2000)
 }
